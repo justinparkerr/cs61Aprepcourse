e6963653cb6cd867db93ebbc925ca58365adca3d
justinparkerr <justinparker@Justins-MacBook-Pro.local>
2016-12-23 01:54:18 -0800

    Ch 5 Things to Try pt 1 and 2

---
+++ Chapter5ThingstoTry1.rb
@@ -0,0 +1,8 @@
+puts 'Hello, what\'s your first name?'
+name=gets.chomp
+puts 'Can you please tell me your middle name aswell?'
+name2=gets.chomp
+puts 'Finally, can I get your last name please?'
+name3=gets.chomp
+puts 'Cool name you got there, ' + name + name2 + name3
+puts 'Nice to meet you, ' + name + name2 + name3
