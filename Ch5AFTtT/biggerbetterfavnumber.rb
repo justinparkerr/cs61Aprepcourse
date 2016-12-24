381d00d3d9eb0ae5f876716ff79969875f91c514
justinparkerr <justinparker@berkeley.edu>
2016-12-24 11:54:03 -0800

    Add files via upload

---
+++ Ch5AFTtT/biggerbetterfavnumber.rb
@@ -0,0 +1,6 @@
+puts 'So how about you tell me your most favorite number in the whole world'
+number1= gets.chomp
+puts 'I must admit, ' +number1+ ' is a pretty sweet number.'
+number2= number1.to_i
+number3= number2+1.to_i
+puts 'However, I do belive that ' + number3.to_s + ' could be a whole lot more worthy of being called one\'s favorite number!'
